6f6f6890e4127d8ad9e0116b32c75c76e25c60d2
josieyao <yao.josephine@yahoo.com>
2019-02-22 16:29:59 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require_relative "./transfer.rb"
+
 class BankAccount
 
+attr_accessor :balance, :status
+attr_reader :name, :broke, :closed
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = 'open'
+        
+    end
+
+    def deposit(balance)
+        @balance = @balance + self.balance
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.balance > 0 && self.status == "open"
+            true
+        else 
+            false
+        end
+    end 
+
+    def close_account
+        self.status = "closed"
+    end
+
 end
+
+#steve_rogers = Hero.new(fbgdbf)
+#Theo_ba = BankAccount.new("nil")
